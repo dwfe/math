0a4f6558e54d370a0d183ee5c1548a8b16807427
z <alex@dwfe.ru>
2022-09-06 19:02:28 +0300

    change of basis

--- src/__tests__/matrix-2d/change-of-basis.test.ts
+++ src/__tests__/matrix-2d/change-of-basis.test.ts
@@ -19,6 +19,9 @@ describe('change of basis', () => {
       WebMatrix.changeOfBasisMatrix(fromBasis, toBasis)
     );
 
+    /**
+     * fromPoint = [2, 1]
+     */
     {
       const fromPoint: TPoint = [2, 1];
       const toPoint = mA.invert().apply(fromPoint);
@@ -28,6 +31,20 @@ describe('change of basis', () => {
       const toPoint: TPoint = [3, -1];
       const fromPoint = mA.apply(toPoint);
       expect(Point.isEqual(fromPoint, [2, 1])).True();
+    }
+
+    /**
+     * fromPoint = [1, -2]
+     */
+    {
+      const fromPoint: TPoint = [1, -2];
+      const toPoint = mA.invert().apply(fromPoint);
+      expect(Point.isEqual(toPoint, [4, -3])).True();
+    }
+    {
+      const toPoint: TPoint = [4, -3];
+      const fromPoint = mA.apply(toPoint);
+      expect(Point.isEqual(fromPoint, [1, -2])).True();
     }
 
 
