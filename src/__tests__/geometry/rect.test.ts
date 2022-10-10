8850db9f8bced219e87b0c2dcc019b3dd44d8139
z <alex@dwfe.ru>
2022-10-10 12:32:04 +0300

    rect

---
+++ src/__tests__/geometry/rect.test.ts
@@ -0,0 +1,29 @@
+import '@do-while-for-each/test'
+import {Point, Rect} from '../../geometry'
+
+describe('rect', () => {
+
+  test('atCenter', () => {
+    const rect = Rect.atCenter([0, 0], 1, 1);
+    expect(rect.left).eq(-0.5);
+    expect(rect.top).eq(-0.5);
+    expect(rect.right).eq(0.5);
+    expect(rect.bottom).eq(0.5);
+  });
+
+  test('4 points', () => {
+    const rect = Rect.atCenter([0, 0], 2, 2);
+    expect(Point.isEqual(Rect.leftTop(rect), [-1, -1])).True();
+    expect(Point.isEqual(Rect.leftBottom(rect), [-1, 1])).True();
+    expect(Point.isEqual(Rect.rightTop(rect), [1, -1])).True();
+    expect(Point.isEqual(Rect.rightBottom(rect), [1, 1])).True();
+  });
+
+  test('center, width, height', () => {
+    const rect = Rect.atCenter([0.5, 0.5], 2, 5);
+    expect(Point.isEqual(Rect.center(rect), [0.5, 0.5])).True();
+    expect(Rect.width(rect)).eq(2);
+    expect(Rect.height(rect)).eq(5);
+  });
+
+});
